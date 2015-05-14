581d6041549d112e68ab7838ded9268f2b2ff1e3
Paweł Buglewicz <illmoded@gmail.com>
2015-05-14 15:43:27 +0200

    jest zrobione żeby równoległe zapisywanie się nie spuło. bez podzielenia wektora protonow na rowne czesci sie nie rusze dalej

--- magnesy.cpp
+++ magnesy.cpp
@@ -1,28 +1,26 @@
-//czas P+K ~1h P20m+10m K2h+niewiemile
-
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <fstream>
 #include <iostream>
-#include <memory> // do unique_ptr
-// #include <typeinfo> //do nazw klas
-#include <boost/thread.hpp>
-// #include <boost/date_time.hpp>
+#include <sstream>
+#include <boost/shared_ptr.hpp>
+#include <thread>
+#include <mutex>
+
 
 using namespace std;
-unsigned int nthreads = boost::thread::hardware_concurrency(); //ilość rdzeni, jest też po prostu w threadzie z c11
 
 class magnes
 {
 public:
 	double polozenie;
 	double dlugosc;
-	double indukcja; // wsp k
+	double indukcja;
 	int name;
 
-	virtual double pole(double x, double y) //trzeba inicjalizować funkcje wirtualne
+	virtual double pole(double x, double y)
 	{
 		return 0;
 	}
@@ -78,6 +76,8 @@ public:
 	double energia;
 };
 
+typedef boost::shared_ptr<magnes> magnes_ptr;
+
 std::vector<proton>wczytajprotony(std::ifstream &plik)
 {
 	std::vector<proton> listap;
@@ -102,12 +102,12 @@ std::vector<proton>wczytajprotony(std::ifstream &plik)
 	return listap;
 }
 
-std::vector<std::unique_ptr<magnes>> wczytajmagnesy(std::ifstream &plik)
+std::vector<magnes_ptr> wczytajmagnesy(std::ifstream &plik)
 {
-	std::vector<std::unique_ptr<magnes>> lista;	
+	std::vector<magnes_ptr> lista;	
 	double a,b,c,d;
-	dipol *dip = new dipol();
-	kwadrupol *kwa = new kwadrupol();
+	magnes_ptr D(new dipol);
+	magnes_ptr K(new kwadrupol);
 
 	int i=0;
 	int j=1;
@@ -116,11 +116,11 @@ std::vector<std::unique_ptr<magnes>> wczytajmagnesy(std::ifstream &plik)
 	{
 		if (d==2)
 		{
-			lista.push_back(std::unique_ptr<magnes>(dip));
+			lista.push_back(D);
 		}
 		else if (d==4)
 		{
-			lista.push_back(std::unique_ptr<magnes>(kwa));
+			lista.push_back(K);
 		}
 		else
 		{
@@ -131,25 +131,91 @@ std::vector<std::unique_ptr<magnes>> wczytajmagnesy(std::ifstream &plik)
 		lista[i]->polozenie=a;
 		lista[i]->dlugosc=b;
 		lista[i]->indukcja=c;
-		i++;j++;
+		i++;
+		j++;
 	}
-
 	return lista;	
 }
 
-// std::vector<std::unique_ptr<magnes>> vappend(std::vector<std::unique_ptr<magnes>> a, std::vector<std::unique_ptr<magnes>> b)
-// {
-// 	a.reserve(a.size() + b.size());
-// 	a.insert(a.end(), std::make_move_iterator(b.begin()), std::make_move_iterator(b.end()));
-// 	b.clear();
-// 	return a;
-// }
+std::vector<magnes_ptr> vappend(std::vector<magnes_ptr> a, std::vector<magnes_ptr> b)
+{
+	a.reserve(a.size() + b.size());
+	a.insert(a.end(), std::make_move_iterator(b.begin()), std::make_move_iterator(b.end()));
+	b.clear();
+	return a;
+}
+
+std::mutex blokada;
+
+void lock(std::string const& msg) //zapisywanie do pliku...
+{
+	blokada.lock();
+    ofstream zapis("output",ios::out);
+
+    zapis << msg;
+
+	zapis.close();  
+    blokada.unlock();
+}
+
+void oblicz(std::vector<magnes_ptr> magnesy,std::vector<proton> protony, double l, double r)
+{
+	
+	double dt=0.001;
+	for (int i = 0; i < protony.size(); i++)
+	{	
+		while(protony[i].z<l && protony[i].x*protony[i].x+protony[i].y*protony[i].y<r*r)
+		{
+			double energiaa=1./protony[i].energia;
+			protony[i].x+=dt*protony[i].px*energiaa;
+			protony[i].y+=dt*protony[i].py*energiaa;
+			protony[i].z+=dt*protony[i].pz*energiaa;
+
+			for (int j = 0; j < magnesy.size(); j++)
+			{
+				while(protony[i].z>magnesy[j]->polozenie && protony[i].z<magnesy[j]->polozenie+magnesy[j]->dlugosc)
+				{	
+					double E=protony[i].energia;		
+					double Bx=magnesy[j]->pole(protony[i].x, protony[i].y);
+					double By=magnesy[j]->pole(protony[i].x, protony[i].y);
+					protony[i].px+=dt/E/E*protony[i].pz*By;
+					protony[i].py+=dt/E/E*protony[i].pz*Bx;
+					protony[i].pz+=dt/E/E*(protony[i].px*By-protony[i].py*Bx);
+					protony[i].x+=dt/E*protony[i].px;
+					protony[i].y+=dt/E*protony[i].py;
+					protony[i].z+=dt/E*protony[i].pz;
+				}
+			}
+		}
+		
+
+		cout << "[" << 1000*i/protony.size()/10. << '%' << "]   " << "\r";
+		if(protony[i].x*protony[i].x+protony[i].y*protony[i].y<r*r)
+		{
+			std::string zapis;
+			stringstream(zapis) << i+1 << "\t" << protony[i].x << "\t" << protony[i].px << "\t" << protony[i].y << "\t" << protony[i].py
+		 	<< "\t" << protony[i].pz << "\t" << protony[i].energia << endl;
+		 	lock(zapis);
+		}
+			
+	}
+}
 
 int main(int argc, char const *argv[])
 {
+
+	unsigned int rdzenie;
+	rdzenie=std::thread::hardware_concurrency();
+
+	if (rdzenie==0)//czyli że nie wykrywa
+	{
+		rdzenie=2;
+	}
+
 	double l=204.0;
 	double r=0.002;
-	std::vector<std::unique_ptr<magnes>> listamagnesow;
+	std::vector<magnes_ptr> listamagnesow;
+	std::vector<magnes_ptr> temp;
 
 	if(argc==1){
 		ifstream plik("def_magn",ios::in);
@@ -159,19 +225,21 @@ int main(int argc, char const *argv[])
 	else 
 		for (int i = 1; i <= argc; i++){
 			ifstream plik(argv[i],ios::in);
-			listamagnesow.reserve(listamagnesow.size()+wczytajmagnesy(plik).size()); 	//cos psuje sie przez to, resize tez nie dziala
-			listamagnesow.insert(listamagnesow.end(), 									//wpisanie konkretnej liczby lub usuniecie powoduje
-				std::make_move_iterator(wczytajmagnesy(plik).begin()), 					//naruszenie ochrony pamieci
-				std::make_move_iterator(wczytajmagnesy(plik).end()));					//wzialem to ze stackoverflow
+			temp = wczytajmagnesy(plik);
+			listamagnesow.reserve(listamagnesow.size()+temp.size());
+			listamagnesow.insert(listamagnesow.end(),
+				std::make_move_iterator(temp.begin()),
+				std::make_move_iterator(temp.end()));
+			temp.clear();
 			plik.close();
 		}
 
 	// cout << listamagnesow.size() << endl;
-	for (int i = 0; i < listamagnesow.size(); i++)
-	{
-		listamagnesow[i]->kto();
-		cout << i+1 << "\t" << listamagnesow[i]->name << endl;
-	}
+	// for (int i = 0; i < listamagnesow.size(); i++)
+	// {
+	// 	listamagnesow[i]->kto();
+	// 	cout << i+1 << "\t" << listamagnesow[i]->name << endl;
+	// }
 
 	ifstream plik("input",ios::in);
 
@@ -181,45 +249,17 @@ int main(int argc, char const *argv[])
 
 	// cout << protony.size() << endl;
 
-	ofstream zapis("output",ios::out);
-
-	double dt=0.001;
-
-	int podzial = protony.size()/nthreads;
-
-	/*for (int i = 0; i < protony.size(); i++)
-	{	
-		while(protony[i].z<l && protony[i].x*protony[i].x+protony[i].y*protony[i].y<r*r)
-		{
-			double energiaa=1./protony[i].energia; // nie jest taka sama...
-			protony[i].x+=dt*protony[i].px*energiaa; //wprowadzenie tutaj dzielenia przez energie spowalnia program niesamowicie
-			protony[i].y+=dt*protony[i].py*energiaa; //mnożenie jest szybsze?
-			protony[i].z+=dt*protony[i].pz*energiaa;
-
-			for (int j = 0; j < listamagnesow.size(); j++)
-			{
-				while(protony[i].z>listamagnesow[j]->polozenie && protony[i].z<listamagnesow[j]->polozenie+listamagnesow[j]->dlugosc)
-				{	
-					//funkcje na pole - jakoś, tak z indukcją to w ogóle działa?
-					double E=protony[i].energia;	//może wywalić, jest taka sama dla wszystkich 			
-					double Bx=listamagnesow[j]->pole(protony[i].x, protony[i].y);
-					double By=listamagnesow[j]->pole(protony[i].x, protony[i].y);
-					protony[i].px+=dt/E/E*protony[i].pz*By; //mnożenie!?!?!?
-					protony[i].py+=dt/E/E*protony[i].pz*Bx;
-					protony[i].pz+=dt/E/E*(protony[i].px*By-protony[i].py*Bx);
-					protony[i].x+=dt/E*protony[i].px;
-					protony[i].y+=dt/E*protony[i].py;
-					protony[i].z+=dt/E*protony[i].pz;
-				}
-			}
-		}
-
-		cout << "[" << 1000*i/protony.size()/10. << '%' << "]   " << "\r";
-		if(protony[i].x*protony[i].x+protony[i].y*protony[i].y<r*r)
-			zapis << i+1 << "\t" << protony[i].x << "\t" << protony[i].px << "\t" << protony[i].y << "\t" << protony[i].py
-		 	<< "\t" << protony[i].pz << "\t" << protony[i].energia << endl;
+	std::thread thr[rdzenie];
+	for (int i = 0; i < rdzenie; ++i)
+	{
+		thr[i]=std::thread(oblicz,listamagnesow,protony,l,r);//ma być tylko 1/rdzenie protonów w wektorze
 	}
 
-	zapis.close();*/
+	for (int i = 0; i < rdzenie; ++i)
+	{
+		thr[i].join();
+	}
+
+
 	return 0;
 }
